2fc77a6464dba3c997a41f9568603096362414a7
AndreaB <9698065@itisrossi.vi.it>
2025-12-09 12:29:26 +0100

    Bot risponde

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,23 @@
 package org.example;
 
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+import java.sql.Array;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+
+
+        try {
+            String botToken = "8330544118:AAEg6Q3jo3wGygviHgw5V1qVngwrKQgnM0k";
+            TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+            botsApplication.registerBot(botToken, new TelegramBot(botToken));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
